b408226c19f3408c5689a19cee4fbd7e04cb6d43
ngxson <thichthat@gmail.com>
2019-04-04 20:25:49 +0200

    add PourMoveTest

---
+++ src/test/PourMoveTest.java
@@ -0,0 +1,33 @@
+package test;
+
+import com.ngxson.programmation.Bottle;
+import com.ngxson.programmation.Configuration;
+import com.ngxson.programmation.move.FillMove;
+import com.ngxson.programmation.move.PourMove;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class PourMoveTest {
+
+    @Test
+    public void display() {
+        List<Bottle> bottles = new ArrayList<>();
+        bottles.add(new Bottle(10, 8, "b1"));
+        bottles.add(new Bottle(10, 5, "b2"));
+        Configuration config = new Configuration(bottles);
+
+        PourMove move = new PourMove(bottles.get(0), bottles.get(1));
+        move.apply(config);
+        move.display();
+        assertEquals(bottles.get(0).getWaterLevel(), 3);
+        assertEquals(bottles.get(1).getWaterLevel(), 10);
+
+        move.reverse(config);
+        assertEquals(bottles.get(0).getWaterLevel(), 8);
+        assertEquals(bottles.get(1).getWaterLevel(), 5);
+    }
+}
