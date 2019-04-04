e60c5bae9331d03e1f0e4af720437909620420e8
ngxson <thichthat@gmail.com>
2019-04-04 19:44:06 +0200

    add BottleTest

---
+++ src/test/BottleTest.java
@@ -0,0 +1,28 @@
+package test;
+
+import com.ngxson.programmation.Bottle;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BottleTest {
+
+    @Test
+    public void pourAllTo() {
+        Bottle bottle1 = new Bottle(10, 8, "b1");
+        Bottle bottle2 = new Bottle(10, 5, "b2");
+
+        bottle1.pourAllTo(bottle2);
+
+        assertEquals(bottle1.getWaterLevel(), 3);
+        assertEquals(bottle2.getWaterLevel(), 10);
+    }
+
+    @Test
+    public void pour() {
+        Bottle bottle = new Bottle(10, 8, "b1");
+        int rest = bottle.pour(22);
+
+        assertEquals(rest, 20);
+    }
+}
