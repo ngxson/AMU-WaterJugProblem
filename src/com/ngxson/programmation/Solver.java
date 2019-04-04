d5e2e97bd1fa780cd3392746f6e028a80109ed1f
ngxson <thichthat@gmail.com>
2019-04-05 00:02:58 +0200

    Solver: init

--- src/com/ngxson/programmation/Solver.java
+++ src/com/ngxson/programmation/Solver.java
@@ -1,4 +1,30 @@
 package com.ngxson.programmation;
 
+import com.ngxson.programmation.move.Move;
+
+import java.util.List;
+
 public class Solver {
+    private Configuration initConfig;
+    private Configuration targetConfig;
+
+    public Solver(Configuration initConfig, Configuration targetConfig) {
+        this.initConfig = initConfig;
+        this.targetConfig = targetConfig;
+    }
+
+    public boolean isWinningMove(Move move) {
+        move.apply(initConfig);
+        boolean isWin = initConfig.equals(targetConfig);
+        move.reverse(initConfig);
+        return isWin;
+    }
+
+    public Move findWinningMove() {
+        List<Move> possibleMoves = initConfig.possibleMoves();
+        for (Move move : possibleMoves) {
+            if (isWinningMove(move)) return move;
+        }
+        return null;
+    }
 }
