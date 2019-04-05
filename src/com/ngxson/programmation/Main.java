a2fe7425239057d3222487ffbb49ceeaa0d2c416
ngxson <thichthat@gmail.com>
2019-04-05 21:10:24 +0200

    Solver: Dijkstra's algorithm

--- src/com/ngxson/programmation/Main.java
+++ src/com/ngxson/programmation/Main.java
@@ -7,18 +7,21 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        exercice6();
+        findSolutionFor2Bottles(3,5, 0, 4);
     }
 
-    private static void exercice6() {
+    private static void findSolutionFor2Bottles(int b1cap,
+                                                int b2cap,
+                                                int b1target,
+                                                int b2target) {
         List<Bottle> bottles1 = new ArrayList<>();
-        bottles1.add(new Bottle(3, 0, "b1"));
-        bottles1.add(new Bottle(5, 0, "b2"));
+        bottles1.add(new Bottle(b1cap, 0, "b1"));
+        bottles1.add(new Bottle(b2cap, 0, "b2"));
         Configuration initConfig = new Configuration(bottles1);
 
         List<Bottle> bottles2 = new ArrayList<>();
-        bottles2.add(new Bottle(3, 3, "b1"));
-        bottles2.add(new Bottle(5, 0, "b2"));
+        bottles2.add(new Bottle(b1cap, b1target, "b1"));
+        bottles2.add(new Bottle(b2cap, b2target, "b2"));
         Configuration targetConfig = new Configuration(bottles2);
 
         Solver solver = new Solver(initConfig, targetConfig);
--- src/com/ngxson/programmation/Solver.java
+++ src/com/ngxson/programmation/Solver.java
@@ -2,15 +2,19 @@ package com.ngxson.programmation;
 
 import com.ngxson.programmation.move.Move;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.List;
 
 public class Solver {
     private Configuration initConfig;
     private Configuration targetConfig;
+    private Deque<Move> candidateMoves;
 
     public Solver(Configuration initConfig, Configuration targetConfig) {
         this.initConfig = initConfig;
         this.targetConfig = targetConfig;
+        this.candidateMoves = new ArrayDeque<>();
     }
 
     public boolean isWinningMove(Move move) {
@@ -20,11 +24,60 @@ public class Solver {
         return isWin;
     }
 
-    public Move findWinningMove() {
+    /**
+     * Based on Dijkstra's algorithm
+     * @param lastMove null if we call the first time
+     * @return the winningMove, null if cannot find
+     */
+    public Move findWinningMove(Move lastMove) {
+        // bump to this node on the tree
+        if (lastMove != null) lastMove.apply();
+        // find all child nodes and check it
         List<Move> possibleMoves = initConfig.possibleMoves();
-        for (Move move : possibleMoves) {
-            if (isWinningMove(move)) return move;
+        for (Move nextMove : possibleMoves) {
+            if (isWinningMove(nextMove)) {
+                // if isWinningMove is true, return it immediately
+                return (lastMove == null)
+                        ? nextMove
+                        : lastMove.andThen(nextMove);
+            } else {
+                // if not, add this child node to queue
+                enqueueNewMoves(lastMove, nextMove);
+            }
         }
+        // go back to root of the tree
+        if (lastMove != null) lastMove.reverse();
+
         return null;
+    }
+
+    /**
+     * Based on Dijkstra's algorithm
+     * @return the winningMove, null if cannot find
+     */
+    public Move findWinningMove() {
+        Move nextMove = null;
+        // repeat for every moves in the queue,
+        // and stop if the queue is empty
+        do {
+            // call findWinningMove recursively
+            // that means we go down 1 stage of the tree
+            Move winningMove = findWinningMove(nextMove);
+            // if we found the winningMove, return it immediately
+            if (winningMove != null) {
+                return winningMove;
+            }
+            nextMove = candidateMoves.pollFirst();
+        } while (nextMove != null);
+
+        return null;
+    }
+
+    private void enqueueNewMoves(Move lastMove, Move nextMove) {
+        candidateMoves.offerLast(
+                (lastMove == null)
+                    ? nextMove
+                    : lastMove.andThen(nextMove)
+        );
     }
 }
